b076e974707f621d984ac7df04b4cc5b1d4206f1
Raul <raul.arriaga.dev@gmail.com>
2021-10-29 21:03:58 +0200

    Estructura del proyecto

---
+++ src/test/java/com/sanvalero/SellAndBuy/SellAndBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanvalero.SellAndBuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellAndBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
