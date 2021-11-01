e18be7ceaacbaff981f2cd74c3b3a19259deaff3
Raúl Arriaga <raul.arriaga.dev@gmail.com>
2021-11-01 10:23:02 +0100

    Creada funcionalidad de añadir pedidos

--- src/main/java/com/sanvalero/SellAndBuy/util/ConstantUtil.java
+++ src/main/java/com/sanvalero/SellAndBuy/util/ConstantUtil.java
@@ -5,6 +5,7 @@ public class ConstantUtil {
     // Mensajes excepciones
     public final static String PRODUCTO_NOT_FOUND = "El producto buscado no existe";
     public final static String PEDIDO_BY_USER_NOT_FOUND = "Este usuario no tiene pedidos";
+    public final static String PEDIDO_NOT_SUCCESS = "No se ha podido registrar el pedido, no tiene productos";
     public final static String USUARIO_NOT_FOUND = "El usuario no existe";
     public final static String USUARIO_DUPLICATE_EXCEPTION = "El usuario ya existe";
     public final static String USUARIO_MISSING_DATA_EXCEPTION = "Por favor, rellena los campos requeridos";
